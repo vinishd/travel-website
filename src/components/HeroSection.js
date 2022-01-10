1e003163ea26a8898036dee7e19639155af65a37
Vinish Dhir <vinish_dhir@yahoo.com>
2022-01-09 22:14:33 -0800

    src change and herosection change

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,11 +2,12 @@ import React from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
+import video from './video-2.mp4';
 
 function HeroSection() {
     return (
         <div className='hero-container'>
-            <video src="/video-2.mp4" autoPlay loop muted />
+            <video src={video} autoPlay loop muted />
             <h1>SEE WHERE WILL YOU RESIDE NEXT</h1>
             <p>What are you waiting for?</p>
             <div className='hero-btns'>
