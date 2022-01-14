c51a670ac0f25d52819f029bcefed65b490b2320
Vinish Dhir <vinish_dhir@yahoo.com>
2022-01-13 21:55:27 -0800

    log in removed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -54,7 +54,7 @@ function Navbar() {
                         </Link>
                     </li>
                 </ul>
-                {button && <Button buttonStyle='btn--outline'>SIGN UP / LOG IN
+                {button && <Button buttonStyle='btn--outline'>SIGN UP
                 </Button>}
               </div>
             </nav>  
